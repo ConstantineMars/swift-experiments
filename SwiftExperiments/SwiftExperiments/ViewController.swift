2e52abbf78a3614c2dd59b6e3823c4a84e9c6f5d
Constantine Mars <constantine.mars@gmail.com>
2019-08-16 08:59:02 -0700

    init

---
+++ SwiftExperiments/SwiftExperiments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftExperiments
+//
+//  Created by Constantine Mars on 8/16/19.
+//  Copyright © 2019 CMars.pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
